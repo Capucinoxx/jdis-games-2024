40473017abb6c70a545c89a6571c238fd6faa02e
Benjamin Courchesne <42392217+Capucinoxx@users.noreply.github.com>
2024-03-24 13:15:16 -0400

    Feat - add auth manager (#7)

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.21.6
 
 require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
-	github.com/google/uuid v1.3.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/influxdata/influxdb-client-go/v2 v2.13.0 // indirect
 	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
---
+++ manager/auth_manager.go
@@ -0,0 +1,71 @@
+package manager
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+// Auth est une interface pour l'authentification des utilisateurs.
+type Auth interface {
+	Register(username string) (string, error)
+	Exist(token string) bool
+}
+
+// AuthManager maintient une liste d'utilisateurs et de jetons d'authentification.
+type AuthManager struct {
+	users map[string]string
+	uuids map[string]string
+	mu    sync.RWMutex
+}
+
+// NewAuthManager crée un nouveau AuthManager.
+func NewAuthManager() *AuthManager {
+	return &AuthManager{
+		users: make(map[string]string),
+		uuids: make(map[string]string),
+	}
+}
+
+// Register enregistre un nouvel utilisateur et retourne un jeton d'authentification.
+// Si l'utilisateur existe déjà, une erreur est retournée. Si l'enregistrement est
+// réussi, le jeton d'authentification est retourné.
+func (am *AuthManager) Register(username string) (string, error) {
+	if _, ok := am.users[username]; ok {
+		return "", fmt.Errorf("user already exists")
+	}
+
+	am.mu.Lock()
+	token := am.uuid()
+	am.users[username] = token
+	am.uuids[token] = username
+	am.mu.Unlock()
+
+	return token, nil
+}
+
+// Exist retourne vrai si le jeton d'authentification existe. Sinon, retourne faux.
+func (am *AuthManager) Exist(token string) bool {
+	am.mu.RLock()
+	_, ok := am.uuids[token]
+	am.mu.RUnlock()
+	return ok
+}
+
+// uuid génère un nouvel identifiant unique universel.
+func (am *AuthManager) uuid() string {
+	return uuid.NewString()
+}
+
+// Users retourne une liste de tous les utilisateurs enregistrés.
+func (am *AuthManager) Users() []string {
+	am.mu.RLock()
+	defer am.mu.RUnlock()
+
+	users := make([]string, 0, len(am.users))
+	for user := range am.users {
+		users = append(users, user)
+	}
+	return users
+}
