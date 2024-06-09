46e8a59d4ba573b050365474b19184e959bf3953
capucinoxx <bendertermebc@gmail.com>
2024-06-09 18:47:59 -0400

    wip

--- internal/manager/round_manager.go
+++ internal/manager/round_manager.go
@@ -1,19 +1,46 @@
 package manager
 
+type Stage uint8
+
 const (
-	ticksPerRound = 5 * 60 * 3
+  discoveryStage Stage = iota
+  pointRushStage
 )
 
+const (
+	ticksPerRound = 5 * 60 * 3
+  ticksPointRushStage = 4 * 60 * 3
+)
+
+type ChangeStageHandler interface {
+  ChangeStage(s Stage)
+}
+
 type RoundManager struct {
+  currentStage Stage
 	ticks int
+  handler ChangeStageHandler
+}
+
+func NewRoundManager(handler ChangeStageHandler) *RoundManager {
+  return &RoundManager{
+    currentStage: discoveryStage,
+    ticks: 0,
+    handler: handler,
+  }
 }
 
 func (r *RoundManager) Restart() {
 	r.ticks = 0
+  r.currentStage = discoveryStage
+  r.handler.ChangeStage(discoveryStage)
 }
 
 func (r *RoundManager) Tick() {
 	r.ticks++
+  if r.ticks == ticksPointRushStage {
+    r.handler.ChangeStage(pointRushStage)
+  }
 }
 
 func (r *RoundManager) HasEnded() bool {
