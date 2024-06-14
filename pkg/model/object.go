9aeafff6ab8304fe4deed4d8eab6cd0fdd665781
capucinoxx <bendertermebc@gmail.com>
2024-06-14 17:18:05 -0400

    wip

---
+++ pkg/model/object.go
@@ -0,0 +1,26 @@
+package model
+
+import "github.com/google/uuid"
+
+type Object struct {
+  uuid [16]byte
+  Position *Point
+  collider *RectCollider
+
+  cleanup bool
+}
+
+func (o *Object) setup(pos *Point, size float32) {
+  o.uuid = uuid.New()
+  o.Position = pos
+  o.collider = NewRectCollider(pos.X, pos.Y, size)
+  o.cleanup = false
+}
+
+func (o *Object) IsCollidingWithPlayer(player *Player) bool {
+  return o.collider.Collisions(player.Collider.polygon())
+}
+
+func (o *Object) Remove() { o.cleanup = true }
+
+func (o *Object) IsAlive() bool { return !o.cleanup }
---
+++ pkg/model/scorer.go
@@ -0,0 +1,11 @@
+package model
+
+type Scorer struct {
+  Object
+  Value int
+}
+
+
+
+
+
