bb16b484226ddf1487bd4c078b96fe1c27b9febd
capucinoxx <bendertermebc@gmail.com>
2024-07-21 02:05:50 -0400

    wip

--- server/internal/handler/middleware.go
+++ server/internal/handler/middleware.go
@@ -12,3 +12,29 @@ func (h *HttpHandler) checkLeaderboardAccess(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (h *HttpHandler) adminOnly(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tkn := r.URL.Query().Get("tkn")
+
+		if tkn == "" {
+			cookie, err := r.Cookie("tkn")
+			if err == nil {
+				tkn = cookie.Value
+			}
+		}
+
+		if tkn == "" {
+			http.Error(w, "admin restricted", http.StatusForbidden)
+			return
+		}
+
+		_, _, isAdmin, _ := h.am.Authenticate(tkn)
+		if !isAdmin {
+			http.Error(w, "admin restricted", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
