8de04a8ed61b027d561c60bcd0548d11adc45322
capucinoxx <bendertermebc@gmail.com>
2024-07-28 20:22:16 -0400

    wip

--- server/pkg/model/weapon.go
+++ server/pkg/model/weapon.go
@@ -72,7 +72,7 @@ func (c *Cannon) Update(players []*Player, dt float64) {
 		p.moveToDestination(dt)
 
 		for _, enemy := range players {
-			if c.owner.Nickname == enemy.Nickname {
+			if c.owner.Nickname == enemy.Nickname || !enemy.IsAlive() {
 				continue
 			}
 
@@ -129,7 +129,7 @@ func (b *Blade) Update(players []*Player, rotation *float64) {
 	}
 
 	for _, enemy := range players {
-		if b.owner.Nickname == enemy.Nickname {
+		if b.owner.Nickname == enemy.Nickname || !enemy.IsAlive() {
 			continue
 		}
 
